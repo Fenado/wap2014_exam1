0b1d023f980c8a9347161adf443a887655c9964d
Bin LI (Richard) <richard.ustc@gmail.com>
2013-06-01 20:12:36 +0800

    Initial commit

---
+++ src/jp/co/wap/exam/Problem1.java
@@ -0,0 +1,15 @@
+package jp.co.wap.exam;
+import java.util.List;
+import jp.co.wap.exam.lib.Interval;
+
+/**
+ * The class Problem1 is to solve the exam1 problem1.
+ */
+public class Problem1 {
+
+    public int getMaxIntervalOverlapCount(List<Interval> intervals){
+        //TODO
+        return 0;
+    }
+
+}
---
+++ src/jp/co/wap/exam/Problem2.java
@@ -0,0 +1,13 @@
+package jp.co.wap.exam;
+import java.util.List;
+import jp.co.wap.exam.lib.Interval;
+
+/**
+ * The class Problem2 is to solve exam1 problem2.
+ */
+public class Problem2 {
+    public int getMaxWorkingTime(List<Interval> intervals){
+        //TODO
+        return 0;
+    }
+}
